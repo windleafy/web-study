dffb415dd44d6e0fc6465c8baf5ed5fe1ad6cea4
windleafy <ydfwind@gmail.com>
2018-06-08 19:39:04 +0800

    php与js通过json相互传参实例
    phpjs0.php
    在一个文件中相互传参
    
    phpjs1.php
    phpjs2.php
    在两个文件中相互传参

---
+++ php-test/phpJs2.php
@@ -0,0 +1,24 @@
+
+<?php
+/*-----此文件echo输出的字符串，将会出现在调用它的html Js脚本内-----*/
+$action=$_GET;
+//$action=$_GET['action'];
+//$action='hello world';//变量可以是字符串、数组、对象等。
+
+$action = array('apple', 'orange', 'banana', 'strawberry');
+$action = json_encode($action);
+echo 'x = '.$action;//此处X没有写Var，表明是Js的全局变量
+
+/*--------------------------------------------------------
+echo "document.write('".$action."')";
+输出结果
+document.write('hello world')
+document.write('["apple","orange","banana","strawberry"]')
+--------------------------------------------------------*/
+
+ 
+//echo '<p align="left" id="windtest"></p>';
+//echo "document.getElementById('windtest').innerHTML='".$action."'";  
+
+//document.getElementById("demo").innerHTML = "段落已修改。"; 
+?>
