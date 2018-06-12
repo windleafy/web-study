097943e60d2948c52c7591496b04e1ebfb7e19a8
windleafy <ydfwind@gmail.com>
2018-06-12 19:31:28 +0800

    数组取值前后台连接测试

---
+++ php-test/PHPArJsJson.php
@@ -0,0 +1,15 @@
+﻿
+<?php
+$action=$_GET;
+$action = array(
+'name'=>'苹果', 
+'nic'=>'果果', 
+'contact'=>array(
+	'email'=>'apple@163.com', 
+	'website'=>'www.163.com'
+	)
+);
+//print_r ($action);
+$action = json_encode($action);
+echo 'str = '.$action;
+?>
