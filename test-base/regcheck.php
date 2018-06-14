5048bd2de14727e86ffca65592d31b77330b4638
windleafy <ydfwind@gmail.com>
2018-06-14 12:50:34 +0800

    追加uuid 追加uuid防止重复的判定

--- test-base/regcheck.php
+++ test-base/regcheck.php
@@ -10,8 +10,9 @@
         $user = $_POST["username"];  
         $psw = $_POST["password"];  
         $psw_confirm = $_POST["confirm"];
+		$uuid = $_POST["uuid"];		echo "<script>alert('".$uuid."');</script>";
 		date_default_timezone_set('PRC');
-		$rgt = date('Y-m-d H:i:s',time());//echo "<script>alert('".$rgt."');</script>";  
+		$rgt = date('Y-m-d H:i:s',time());	//echo "<script>alert('".$rgt."');</script>";  
         if($user == "" || $psw == "" || $psw_confirm == "")  
         {  
             echo "<script>alert('请确认信息完整性！'); history.go(-1);</script>";  
@@ -30,7 +31,7 @@
 					// 设置 PDO 错误模式为异常
 					$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 					
-					//重名判定-开始
+					// 重名判定-开始
 					$sql = "SELECT username FROM user WHERE username = '$_POST[username]'";
 					foreach ($conn->query($sql) as $row) {//echo "<script>alert('hello');</script>";
 					//print $row['userName'] . "\n"  能够进入此循环，表示有重名;
@@ -38,7 +39,18 @@
 						echo "<script>alert('重名');</script>";
 						header("Refresh:0.1;url=register.php");
 						exit;
-					}//重名判定-结束
+					}// 重名判定-结束
+
+					// uuid校验-开始
+					//$uuid = '1'; //检测uuid是否重复的设定常量;
+					$sql = "SELECT id FROM user WHERE id = '$_POST[uuid]'";
+					foreach ($conn->query($sql) as $row) {
+						$conn = null;
+						echo "<script>alert('系统有些许忙碌，客官请稍后');</script>";
+						header("Refresh:0.1;url=register.php");
+						exit;
+					}
+					// uuid校验-结束
 
 					// 预处理 SQL 并绑定参数
 					$stmt = $conn->prepare("INSERT INTO user (id, phone, gold, username, password, regtime) 
@@ -51,7 +63,8 @@
 					$stmt->bindParam(':regtime', $regtime);
 
 					// 插入一条数据
-					$id = mt_rand(0,mt_getrandmax());
+					//$id = mt_rand(0,mt_getrandmax());
+					$id = $uuid;	//echo "<script>alert('002".$uuid."');</script>";
 					$phone = "1333333333";
 					$gold = "123456789";
 					$username = $user;
