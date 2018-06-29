64af54419bac098d91eaf6be735ff6c5751ac7c4
windleafy <ydfwind@gmail.com>
2018-06-29 15:45:02 +0800

    loadNewSlides() 函数合并成一个

--- js/swiper.js
+++ js/swiper.js
@@ -17,65 +17,35 @@ var mySwiper = new Swiper('.swiper-container',{
       mySwiper.params.onlyExternal=true
       $('.preloader').addClass('visible');
 	loadNewSlides();
-	loadNewSlides2();
 	}
   }
 })
 var slideNumber = 0;
 function loadNewSlides(){
-	if (currentGroup<groupNum){//此处为是否有新条目的开关
-		setTimeout(function(){
-		  //Prepend new slide
-		  var colors = ['red','blue','green','orange','pink'];
-		  var color = colors[Math.floor(Math.random()*colors.length)];
-
-		  var x = currentGroup;
-		  //mySwiper.prependSlide('<div '+data.join('')+' id="table'+x+'" style="border: 1px solid black" value="'+currentGroup+'">jphtml.com '+slideNumber+'</div>');	
-		  mySwiper.prependSlide('<div id="table'+x+'" style="border: 1px solid black" value="'+currentGroup+'">jphtml.com '+slideNumber+'</div>');	
-		
-		  createTable1();
-
+	//alert(currentGroup);alert(groupNum);
+	var table = 0;		//0 处理主页对战列表；//1 处理记录页列表
+	crtGroup = currentGroup;
+	grpNum = groupNum;
+	if ((currentGroup==0)&&(groupNum==0)){
+		crtGroup = currentGroup2;
+		grpNum = groupNum2;
+		table = 1;	
+	}
 	
-		  //Release interactions and set wrapper
-		  mySwiper.setWrapperTranslate(0,0,0)
-		  mySwiper.params.onlyExternal=false;
-		  //Update active slide
-		  mySwiper.updateActiveSlide(0)
-		  //Hide loader
-		  $('.preloader').removeClass('visible');
-		},1000)
-		slideNumber++;
-	}
-	else{
-		setTimeout(function(){
-		  //Prepend new slide
-		  //var colors = ['red','blue','green','orange','pink'];
-		  //var color = colors[Math.floor(Math.random()*colors.length)];
-		  //mySwiper.prependSlide('<div class="title">jphtml.com '+slideNumber+'</div>', 'swiper-slide '+color+'-slide');
-		  //Release interactions and set wrapper
-		  mySwiper.setWrapperTranslate(0,0,0)
-		  mySwiper.params.onlyExternal=false;
-		  //Update active slide
-		  mySwiper.updateActiveSlide(0)
-		  //Hide loader
-		  $('.preloader').removeClass('visible');
-		},1000)
-		//slideNumber++;
-	}
-}
-function loadNewSlides2(){
-	if (currentGroup2<groupNum2){//此处为是否有新条目的开关
+	if (crtGroup<grpNum){//此处为是否有新条目的开关
 		setTimeout(function(){
 		  //Prepend new slide
 		  var colors = ['red','blue','green','orange','pink'];
 		  var color = colors[Math.floor(Math.random()*colors.length)];
 
-		  var x = currentGroup2;
+		  var x = crtGroup;
 		  //mySwiper.prependSlide('<div '+data.join('')+' id="table'+x+'" style="border: 1px solid black" value="'+currentGroup+'">jphtml.com '+slideNumber+'</div>');	
-		  mySwiper.prependSlide('<div id="table'+x+'" style="border: 1px solid black" value="'+currentGroup2+'">jphtml.com '+slideNumber+'</div>');	
-		
-		  createTable2();
-
+		  mySwiper.prependSlide('<div id="table'+x+'" style="border: 1px solid black" value="'+crtGroup+'">jphtml.com '+slideNumber+'</div>');	
+		  
+		  if( table==0){createTable1();}
+		  else{createTable2();}
+		  
+			
 	
 		  //Release interactions and set wrapper
 		  mySwiper.setWrapperTranslate(0,0,0)
