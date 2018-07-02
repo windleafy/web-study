819e1017b1de0e910439beb155d9de9f8078b19b
windleafy <ydfwind@gmail.com>
2018-07-02 08:58:31 +0800

    去除多余引用

--- record.php
+++ record.php
@@ -9,23 +9,18 @@ session_start();
 		header("location:login.html");
 	}
 ?>
-
 <html lang="cn">
 <head>
 <meta charset="utf-8">
 <title>Home-record</title>
 <!-- meta tags -->
 
-
-
 <!-- stylesheet -->
-<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
 <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
-<link rel="stylesheet" href="css/idangerous.swiper.css">
+<link href="css/idangerous.swiper.css" rel="stylesheet">
 <script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
 <script type="text/javascript" src="js/gameinfo.js">/*-- 比赛信息处理 --*/</script>
 <script type="text/javascript" src="php/getUserBet.php">/*此处取回games的值*/</script> 	
-	
 <script>/*--窗口自适应处理--*/
 window.onload=function (){
 	//--屏幕高度自适应处理	alert('window.screen.height：'+window.screen.height);
@@ -72,20 +67,12 @@ window.onload=function (){
 				</div>                                                                
 			</div>
 		</div>
-
 	<!--主页内容结束-->
 
 <!--swiper-begin-->
 <script src="js/idangerous.swiper.min.js"></script>
 <script src="js/swiper.js"></script> 
-
-<script>
-//赋初值
-//createTable2();//gameinfo.js
-</script>
 <!--swiper-end-->
-
-<script type="text/javascript" src="js/bootstrap.js"></script>
 
 </body>
 </html>
