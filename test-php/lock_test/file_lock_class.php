e9bf448ed696b152e1ec22e3d6addddc58bb4287
windleafy <ydfwind@gmail.com>
2018-07-19 23:49:46 +0800

    提交文件锁类完成模型

--- test-php/lock_test/file_lock_class.php
+++ test-php/lock_test/file_lock_class.php
@@ -1,6 +1,6 @@
 <?php
 header("Content-type: text/html; charset=utf-8");
-
+/*
 $fp = fopen('./lock.txt','r');
 $try = 10;  //声明一个变量表示要获取的次数，防止死循环
 do{
@@ -43,6 +43,7 @@ if($lock) {
 			$x=1;
 			$stmt = $conn->prepare('UPDATE warehouse SET stock = '.$new_stock. " WHERE id = ".$x);
 			$stmt->execute();//减库存操作
+			echo 'do it';
 		}
 	}
 	catch(PDOException $e)
@@ -57,18 +58,70 @@ else{
 	die('系统繁忙！'); 
 }
 
+*/
+useLock();
+function useLock(){
+    $lockobj = new fileLock('lockname');	//创建文件锁类
+    $lock = $lockobj->Lock('lockname');		//调用加锁函数
+    if(!$lock){
+        echo "lockname is lock";
+        return;
+    }
+
+    /*  热点代码 写在这里--开始
+     */
+	$servername = "localhost";
+	$username = "root";
+	$password = "root";
+	// 创建连接
+	try {
+		$conn = new PDO("mysql:host=$servername;dbname=ydfdbpdo", $username, $password);
+		echo "连接成功<br>"; 
+		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$stmt = $conn->prepare("SELECT stock FROM warehouse"); 
+		$stmt->execute();
+		// 设置结果集为关联数组
+		$result = $stmt->setFetchMode(PDO::FETCH_ASSOC); 		
+
+		foreach(($stmt->fetchAll()) as $k=>$v) { 
+			$ret[]=$v;
+		}
+
+		$stock=$ret[0]["stock"];
+		if( $stock < 1) {
+			die('<br>库存不足');
+		}
+		else{
+			$new_stock = $stock - 1;
+			$x=1;
+			$stmt = $conn->prepare('UPDATE warehouse SET stock = '.$new_stock. " WHERE id = ".$x);
+			$stmt->execute();//减库存操作
+			//echo 'do it';
+		}
+	}
+	catch(PDOException $e)
+	{
+		echo $e->getMessage();
+	}	
+    /*  热点代码 写在这里--结束
+     */	
+	echo 'do it next';
+	//sleep(2);
+    $lockobj->unLock('lockname');		//调用解锁函数
+}
+
 
 class fileLock{
     const KEY_PREFIX = 'lock_';
     private $fp;
-    public function __construct(){
+    public function __construct($lock_id){
         $fkey = self::KEY_PREFIX.$lock_id; 
 		//echo '<br>$mkey:'.$mkey.'<br>';
-		$this->fp = fopen('./'.$fkey,'r');	//此处需要根据锁名，创建文件
+		$this->fp = fopen('./'.$fkey,'w');	//此处需要根据锁名，创建文件
     }
 
     /**
-     * 进行锁操作
+     * 进行锁操作--加锁函数
      * @param [type]  $lock_id
      * @param integer $expire
      */
